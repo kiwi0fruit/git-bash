e4b3adf177f64a0fed95e2ba5156e256ac14d164
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-18 00:32:46 +0700

    commit

---
+++ pypi/bash-git-win/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bash-git-win',
+    version='0.1',
+
+    description='Stub that prevents name clash with https://github.com/conda-forge/bash-git-win-feedstock For Bash from Git see https://github.com/kiwi0fruit/py-bash-git-win',
+    url='https://github.com/kiwi0fruit/py-bash-git-win',
+    author='kiwi0fruit',
+    author_email='peter.zagubisalo@gmail.com',
+    license='MIT',
+)
