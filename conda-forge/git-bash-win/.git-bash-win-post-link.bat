ef34541ed6f16660b1e61bcc2258457eeceb273e
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-19 17:31:55 +0700

    commit

--- conda-forge/git-bash-win/.git-bash-win-post-link.bat
+++ conda-forge/git-bash-win/.git-bash-win-post-link.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 for %%a in ("%~dp0") do for %%b in ("%%~dpa.") do set _root=%%~dpb
+endlocal
 :: %_root%Some_Dir\This_Script.bat
 cd /d "%_root%Library\git-bash-win" || exit 1
 git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat
