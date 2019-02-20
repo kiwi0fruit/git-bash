04632ffc5c589e5b7c367284ba1f18a34cac425a
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-20 16:39:44 +0700

    commit

--- conda-forge/git-bash-win/.git-bash-win-post-link.bat
+++ conda-forge/git-bash-win/.git-bash-win-post-link.bat
@@ -1,10 +1,6 @@
 @echo off
-:: debug:
-chcp 65001 > nul
-set "LANG=C.UTF-8"
-::
 set "_bash=%PREFIX%\Library\git-bash-win"
 set "PATH=%_bash%\cmd;%PATH%"
 cd /d "%_bash%" || exit 1
-::git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat
-call .\post-install.bat
+git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat || exit 1
+del "%_bash%\post-install.bat" || exit 1
