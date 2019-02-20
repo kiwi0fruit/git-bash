b7d89826324e238adefefd76d84f9d1372f579bf
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-21 01:51:35 +0700

    commit

--- pypi/git-bash/setup.py
+++ pypi/git-bash/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='git-bash-win',
+    name='git-bash',
     version='0.1',
 
-    description='Stub that prevents name clash with https://github.com/conda-forge/git-bash-win-feedstock For Git Bash see https://github.com/kiwi0fruit/py-bash',
+    description='Stub that prevents name clash with https://github.com/conda-forge/git-bash-feedstock For Git Bash see https://github.com/kiwi0fruit/py-bash',
     url='https://github.com/kiwi0fruit/py-bash',
     author='kiwi0fruit',
     author_email='peter.zagubisalo@gmail.com',
