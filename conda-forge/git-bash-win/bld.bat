376de727dfbd3391e10a28ff69fd0aa57f61b3a5
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-20 15:51:35 +0700

    commit

--- conda-forge/git-bash-win/bld.bat
+++ conda-forge/git-bash-win/bld.bat
@@ -5,7 +5,7 @@ mkdir "%_bash%" || exit 1
 copy "%_bash%\LICENSE.txt" .\ || exit 1
 :: Test Scripts\ disappearance:
 del "%_bash%\etc\post-install\99-post-install-cleanup.post" || exit 1
-patch -i "%RECIPE_DIR%\01-devices.post.patch" "%_bash%\etc\post-install\01-devices.post"
+patch -i "%RECIPE_DIR%\01-devices.post.patch" "%_bash%\etc\post-install\01-devices.post" || exit 1
 
 if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin" || exit 1
 copy "%RECIPE_DIR%\bash.bat" "%PREFIX%\bin\" || exit 1
