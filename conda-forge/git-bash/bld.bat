8e0f3842268a3320c5e0e7b784cf833fb29f166c
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-22 23:04:36 +0700

    commit

--- conda-forge/git-bash/bld.bat
+++ conda-forge/git-bash/bld.bat
@@ -3,7 +3,7 @@ mkdir "%_bash%" || exit 1
 7za x PortableGit-%PKG_VERSION%-%ARCH%-bit.7z.exe -o"%_bash%" -aoa || exit 1
 
 copy "%_bash%\LICENSE.txt" .\ || exit 1
-patch -i "%RECIPE_DIR%\01-devices.post.patch" "%_bash%\etc\post-install\01-devices.post" || exit 1
+::patch -i "%RECIPE_DIR%\01-devices.post.patch" "%_bash%\etc\post-install\01-devices.post" || exit 1
 patch -i "%RECIPE_DIR%\post-install.bat.patch" "%_bash%\post-install.bat" || exit 1
 
 del "%_bash%\etc\post-install\01-devices.post" || exit 1
