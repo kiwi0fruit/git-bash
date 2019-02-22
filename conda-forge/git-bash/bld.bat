6c90604842df8a6896ab8f76684c2d39221d01f0
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-23 04:30:48 +0700

    commit

--- conda-forge/git-bash/bld.bat
+++ conda-forge/git-bash/bld.bat
@@ -14,30 +14,9 @@ move "%_post%\99-post-install-cleanup.post" "%_temp_post%\" || exit 1
 
 :: post-install:
 set "_CWD=%CD%"
-cd /d "%_bash_dir%" || exit /b 1
-set "_PATH=%PATH%"
-set "PATH=%CD%\cmd;%PATH%"
-::.\git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat > __stdout__ 2> __stderr__
-call .\post-install.bat > __stdout__ 2> __stderr__
-if not %errorlevel%==0 (
-    type __stderr__ 1>&2
-    type __stdout__
-    del __stdout__ && del __stderr__
-    exit /b %errorlevel%
-)
-set "_err="
-set /p _err=<__stderr__
-if defined _err if not "%_err%" == "" (
-    type __stderr__ 1>&2
-    type __stdout__
-    del __stdout__ && del __stderr__
-    exit /b 1
-)
-if exist __stdout__ del __stdout__ || exit /b 1
-if exist __stderr__ del __stderr__ || exit /b 1
-cd /d "%_CWD%"
-set "PATH=%_PATH%"
-set "_err=" && set "_CWD=" && set "_PATH="
+cd /d "%_bash_dir%" || exit 1
+.\git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat || exit 1
+cd /d "%_CWD%" || exit 1
 ::
 
 rmdir /s "%_post%" || exit 1
