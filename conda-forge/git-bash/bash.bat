abea788072c3d6d202967700810cc674c9a21652
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-24 01:27:34 +0700

    commit

--- conda-forge/git-bash/bash.bat
+++ conda-forge/git-bash/bash.bat
@@ -2,13 +2,19 @@
 if not exist "%~dp0\..\Library\git-bash\post-install.bat" goto skip
 
 :: post-install:
-set "_cwd=%CD%" && cd /d "%~dp0\..\Library\git-bash" || exit 1
+set "_cwd=%CD%" && cd /d "%~dp0\..\Library\git-bash" || exit /b 1
 set "_err="
 .\git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat > _out 2> _err ^
-|| set "_err=true" && cd /d "%CD%"
+    || set "_err=1" && cd "%CD%"
 for /f %%i in ("_err") do set _size=%%~zi
-if %_size% gtr 0 set "_err=true"
+if %_size% gtr 0 set "_err=1"
 set "_size="
+del post-install.bat ^
+    || set "_err=1" && cd "%CD%"
+rmdir /s /q etc\post-install ^
+    || set "_err=1" && cd "%CD%"
+cmd /c bin\bash.exe -c "echo hello | cp /dev/stdin /dev/stdout" ^
+    || set "_err=1" && cd "%CD%"
 if defined _err (
     echo git bash post install failed 1>&2
     type _err 1>&2
@@ -17,10 +23,8 @@ if defined _err (
 ) else (
     echo @exit /b 0 > "%~dp0\git-bash-post-install.bat"
 )
-del post-install.bat || exit /b 1
-rmdir /s /q .\etc\post-install || exit /b 1
-del _out || exit /b 1
-del _err || exit /b 1
+del _out || cd "%CD%"
+del _err || cd "%CD%"
 cd /d "%_cwd%"
 set "_cwd="
 if defined _err set "_err=" && exit /b 1
