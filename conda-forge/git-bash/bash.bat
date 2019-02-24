6480a1189d82bb54694065eb0b942e46b0147068
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-24 21:36:49 +0700

    commit

--- conda-forge/git-bash/bash.bat
+++ conda-forge/git-bash/bash.bat
@@ -1,20 +1,41 @@
 @echo off
 if not exist "%~dp0\..\Library\git-bash\post-install.bat" goto skip
 
-:: post-install:
+:: --------------------------------------------------
+:: Post install that determines if it was successful:
+:: --------------------------------------------------
 set "_cwd=%CD%" && cd /d "%~dp0\..\Library\git-bash" || exit /b 1
+
+:: First run of the post install:
 set "_err="
 .\git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat > _out 2> _err ^
     || set "_err=1" && cd "%CD%"
 for /f %%i in ("_err") do set _size=%%~zi
 if %_size% gtr 0 set "_err=1"
 set "_size="
+
+:: Second /dev/stdin bugfix run of the post install:
+rmdir /s /q etc\post-install ^
+    || set "_err=1" && cd "%CD%"
+mkdir etc\post-install ^
+    || set "_err=1" && cd "%CD%"
+move 01-devices.post etc\post-install\01-devices.post ^
+    || set "_err=1" && cd "%CD%"
+start "bash" .\git-bash.exe --hide
+timeout 5 > nul
+taskkill /f /im bash.exe > nul || cd "%CD%"
+
+:: First clean up after post install:
 del post-install.bat ^
     || set "_err=1" && cd "%CD%"
 rmdir /s /q etc\post-install ^
     || set "_err=1" && cd "%CD%"
+
+:: Test /dev/stdin bug:
 .\bin\bash.exe -c "if [[ $(echo hello | cp /dev/stdin /dev/stdout) != 'hello' ]]; then echo 'bad /dev/stdin' 1>&2; exit 1; fi" ^
     > nul || set "_err=1" && cd "%CD%"
+
+:: Second clean up after post install:
 if defined _err (
     echo git bash post install failed 1>&2
     type _err 1>&2
@@ -27,9 +48,14 @@ del _out || cd "%CD%"
 del _err || cd "%CD%"
 cd /d "%_cwd%"
 set "_cwd="
+
+:: Exit with error if post install failed:
 if defined _err set "_err=" && exit /b 1
 
+:: -----------------
+:: Running bash.exe:
+:: -----------------
 :skip
 "%~dp0\..\Library\git-bash\bin\%~n0.exe" %*
-:: the first successful run of %~n0.bat would overwrite it:
-echo @"%%~dp0\..\Library\git-bash\bin\%~n0.exe" %%* > "%~dp0\%~n0.bat"
+:: First successful run of the %~f0 would overwrite it:
+echo @"%%~dp0\..\Library\git-bash\bin\%~n0.exe" %%* > "%~f0"
