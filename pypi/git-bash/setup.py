3f9bb9e6ab35adf3ea6c2bedfa3e19355099114c
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2019-07-19 12:22:47 +0700

    description fix -> v0.2

--- pypi/git-bash/setup.py
+++ pypi/git-bash/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='git-bash',
-    version='0.1',
+    version='0.2',
 
-    description='Stub that prevents name clash with https://github.com/conda-forge/git-bash-feedstock For Git Bash see https://github.com/kiwi0fruit/py-bash',
-    url='https://github.com/kiwi0fruit/py-bash',
+    description='Stub that prevents name clash with https://github.com/conda-forge/git-bash-feedstock For Git Bash see https://github.com/kiwi0fruit/git-bash',
+    url='https://github.com/kiwi0fruit/git-bash',
     author='kiwi0fruit',
     author_email='peter.zagubisalo@gmail.com',
     license='MIT',
