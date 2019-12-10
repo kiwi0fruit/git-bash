c141656f1433804421e04d6d16e1eb8b7210e180
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-12-10 13:33:02 +0700

    2.24.0

--- conda-forge/git-bash/.git-bash-pre-unlink.bat
+++
@@ -1,4 +0,0 @@
-rmdir /s /q "%PREFIX%\Library\git-bash\dev" || cd "%CD%"
-rmdir /s /q "%PREFIX%\Library\git-bash\etc" || cd "%CD%"
-del "%PREFIX%\Library\git-bash\mingw64\libexec\git-core\dlls-copied.exe" || cd "%CD%"
-rmdir /s /q "%PREFIX%\Library\git-bash" || cd "%CD%"
